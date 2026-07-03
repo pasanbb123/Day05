5df4001047565118810d6d9fbbf68aba3936607c
pasanbb123 <pasanthegrate@gmail.com>
2026-07-03 23:02:37 +0530

    js arithmatic op

--- js/app.js
+++ js/app.js
@@ -1,6 +1,51 @@
-let name= "Saman";
-let age = 18;
-console.log(name);
-console.log(age);
-console.log(typeof name );
-console.log(typeof age);
+// let name = "Saman";
+
+// let age = 18;
+
+// name = "ranil";
+
+// console.log(name);
+// console.log(age);
+
+// alert("Hello icet");
+
+
+// String name = "10";
+
+// ---------------------------------
+
+// let name = "saman";
+// let age = 18;
+// let isAfterAL = false;
+// let customers = [];
+// let customer = {};
+// let salary = 7500.0;
+
+// console.log(typeof name);
+
+// --------------------------------------------
+//  arithmetic operators
+
+// + | - | * | /
+
+// let x = 10;
+// let y = 20;
+
+// let z = x / y;
+
+// console.log(z);
+
+// -------------------------------------------
+
+//  comparison operators
+
+// > | < | >= | <= | == | !=
+// >== | <== | === | !== if u want to check the 
+// both value and type
+
+
+// let x = 11;
+
+// let y = "11";
+
+// console.log(x===y);
