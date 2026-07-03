5763b964e53ad99c932d209eba6255b16f407738
pasanbb123 <pasanthegrate@gmail.com>
2026-07-03 23:20:26 +0530

    switch case

--- js/app.js
+++ js/app.js
@@ -74,6 +74,25 @@ if (x > y) {
 }else if ( x< y) {
     console.log("x is lesser than y");
 }else{
-    console.log("x is equal to y");
-    
+    console.log("x is equal to y");    
 }
+
+// switch statement
+
+ let day = 1;
+
+ switch (day) {
+     case 1:
+         console.log("Monday");
+         break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    default:
+        console.log("Invalid day");
+}
+
+// -------------------------------------------
